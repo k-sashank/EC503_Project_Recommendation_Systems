e539a1992e9d2cfb0ddc92e620662889f27e8105
Madipally Sai Krishna Sashank <madipallysashank@gmail.com>
2023-04-06 17:19:50 -0400

    Delete Jaccard Similarity/Music directory

--- Jaccard Similarity/Music/jaccard_similarity.m
+++
@@ -1,5 +0,0 @@
-function jaccard_sim = jaccard_similarity(set1, set2)
-    intersection_set = intersection_finder(set1, set2);
-    union_set = union_finder(set1, set2);
-    jaccard_sim = length(intersection_set) / length(union_set);
-end
--- Jaccard Similarity/Music/union_finder.m
+++
@@ -1,9 +0,0 @@
-function union_set = union_finder(set1, set2)
-    union_set = set1;
-    for i = 1:length(set2)
-        elem = set2(i);
-        if ~ismember(elem, union_set)
-            union_set(end+1) = elem;
-        end
-    end
-end
